d0871ea0b0b6b09a8dce1771dcd76f261fc62ead
MacoChave <201020831@ingenieria.usac.edu.gt>
2022-08-12 20:41:12 +0000

    Nombre y código de modulos implementado

---
+++ modules/proc_mod.c
@@ -0,0 +1,62 @@
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/sched/signal.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("sopes2");
+MODULE_DESCRIPTION("Basic process information Linux module.");
+MODULE_VERSION("0.01");
+
+static int writeFile(struct seq_file *archivo, void *v)
+{
+
+    seq_printf(archivo, "==============================\n");
+    seq_printf(archivo, "=             OS2            =\n");
+    seq_printf(archivo, "=            sopes2          =\n");
+    seq_printf(archivo, "=           proc_mod         =\n");
+    seq_printf(archivo, "==============================\n");
+
+    return 0;
+}
+
+static int atOpen(struct inode *inode, struct file *file)
+{
+    return single_open(file, writeFile, NULL);
+}
+
+static const struct proc_ops ops = {
+    .proc_open = atOpen,
+    .proc_read = seq_read};
+
+int proc_count(void)
+{
+    int i = 0;
+    struct task_struct *thechild;
+    for_each_process(thechild)
+    {
+        pr_info("== %s [%d]\n", thechild->comm, thechild->pid);
+        i++;
+    }
+    return i;
+}
+
+static int load_module(void)
+{
+    printk(KERN_INFO "Total running processes: %d .\n", proc_count());
+
+    proc_create("proc_mod", 0, NULL, &ops);
+    return 0;
+}
+
+static void unload_module(void)
+{
+    printk(KERN_INFO "Goodbye!\n");
+
+    remove_proc_entry("proc_mod", NULL);
+}
+
+module_init(load_module);
+module_exit(unload_module);
