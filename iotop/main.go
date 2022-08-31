b4be606e12702847169da6c11f0c5b6eeb4355fc
LuisMorales25 <0108moralesluis@gmail.com>
2022-08-31 04:02:45 +0000

    implementacion de strace

--- iotop/main.go
+++ iotop/main.go
@@ -6,9 +6,13 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"syscall"
+	"bufio"
+	"strings"
 )
 
 var userName string
+
 
 func main() {
 	// MAIN MENU
@@ -87,7 +91,8 @@ func selectCommands() {
 	menu := make(map[string]string)
 	menu["1"] = "IOTOP"
 	menu["2"] = "TOP"
-	menu["3"] = "Regresar"
+	menu["3"] = "STRACE"
+	menu["4"] = "Regresar"
 	for {
 		for k, v := range menu {
 			println(k, v)
@@ -101,7 +106,10 @@ func selectCommands() {
 			cmdIOTOP()
 		case "2":
 			cmdTOP()
-		case "3":
+		case "3":	
+			
+			cmdSTRACE()
+		case "4":
 			println("Salir")
 			userName = ""
 			clearConsole()
@@ -174,8 +182,88 @@ func cmdTOP() {
 
 }
 
+func cmdSTRACE() {
+		var answer string
+	
+	for {
+		for {
+			println("***             INGRESE UN COMANDO                       ***")
+			com := bufio.NewScanner(os.Stdin)
+			if com.Scan() {
+				println("**************************************************************************")
+				println("***                              STRACE SYSTEM                         ***")
+				println("**************************************************************************")
+				fmt.Println("*** 						USUARIO: 					  ***", userName)
+				println("**************************************************************************\n")
+				strace(strings.Fields(com.Text()))
+				break;
+			}
+		}
+		
+		println("¿Ejecutar de nuevo? (y/n): ")
+		fmt.Scanln(&answer)
+		if answer == "n" || answer == "N" {
+			clearConsole()
+			break
+		} else if answer == "y" || answer == "Y" {
+			continue
+		} else {
+			println("Respuesta no válida.")
+		}
+	}
+
+}
+
 func clearConsole() {
 	c := exec.Command("clear")
 	c.Stdout = os.Stdout
 	c.Run()
+}
+
+func strace(command []string){
+	var regs syscall.PtraceRegs
+	var ss syscallCounter
+
+	ss = ss.init()
+
+	cmd := exec.Command(command[0], command[1])
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Ptrace: true,
+	}
+
+	cmd.Start()
+	err := cmd.Wait()
+	if err != nil {
+		fmt.Printf("Wait returned: %v\n", err)
+	}
+
+	pid := cmd.Process.Pid
+	exit := true
+
+	for {
+		if exit {
+			err = syscall.PtraceGetRegs(pid, &regs)
+			if err != nil {
+				break
+			}
+			ss.inc(regs.Orig_rax)
+		}
+
+		err = syscall.PtraceSyscall(pid, 0)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = syscall.Wait4(pid, nil, 0, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		exit = !exit
+	}
+
+	ss.print()
 }
