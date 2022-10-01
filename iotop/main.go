8883552b89bc33e6fbc58e79cc6896cde7ddbccd
A <201318652@ingenieria.usac.edu.gt>
2022-10-01 04:36:33 -0600

    le 0

--- iotop/main.go
+++ iotop/main.go
@@ -381,7 +381,7 @@ func memsim(ciclos int, unidades string) {
 	for i := 1; i <= ciclos; i++ {
 		var wg sync.WaitGroup // Declarando nuestro wait group
 		fmt.Println("::::::::::::	Ciclo de trabajo: ", i, "	::::::::::::")
-		for j := size - 1; j > 0; j-- {
+		for j := size - 1; j >= 0; j-- {
 			wg.Add(1) // Indicamos la cantidad de rutinas a esperar
 			process := 1
 			value := units_arr[j]
