270a466a50aac61dfe5513f343de7488d9474a58
Marco Chávez <201020831@ingenieria.usac.edu.gt>
2022-10-28 01:51:57 -0600

    Solucion a environment

--- server/configs/env.go
+++ server/configs/env.go
@@ -1,26 +1,21 @@
 package configs
 
-import (
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
-)
+import "os"
 
 func EnvMongoUser() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	return os.Getenv("MONGO_USER")
 }
 
 func EnvMongoPass() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	return os.Getenv("MONGO_PASS")
 }
